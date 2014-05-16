9ffc3201b944274402862c66364deae75a65f030
Igor Gueths <igor.gueths@rackspace.com>
2014-05-15 20:22:04 -0500

    Initial commit and repository init.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'barbican-monitoring'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures barbican-monitoring'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
