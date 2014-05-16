b2bc78226959d75626846a5cb6464fba741be0a3
Igor Gueths <igor.gueths@rackspace.com>
2014-05-16 13:39:39 -0500

    Attribute change.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@
 default['barbican-monitoring']['install-agent'] = false
 default['barbican-monitoring']['notification-plan'] = ''
 # Default path to our symmetric key for Rackspace data bag decryption.
-default['barbican-monitoring']['credentials_path'] = '/etc/chef/credentials_key'
+default['barbican-monitoring']['credentials_path'] = '/etc/chef/keys'
 default['barbican-monitoring']['node_type'] = ''
