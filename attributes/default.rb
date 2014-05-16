3d37eeb9b09a473fc5ad2f5315f72b08c523823c
Igor Gueths <igor.gueths@rackspace.com>
2014-05-15 21:14:05 -0500

    Added attributes file.

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+# Cookbook name: Barbican-monitoring
+# Default attributes for this wrapper.
+default['barbican-monitoring']['install-agent'] = false
+default['barbican-monitoring']['notification-plan'] = ''
