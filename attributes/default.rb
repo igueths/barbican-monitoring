cbcd13729812802914ae310fc8b7891587e2742b
Igor Gueths <igor.gueths@rackspace.com>
2014-05-15 21:24:42 -0500

    Added default attribute for the path to the Rackspace data bag decryption key.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@
 # Default attributes for this wrapper.
 default['barbican-monitoring']['install-agent'] = false
 default['barbican-monitoring']['notification-plan'] = ''
+# Default path to our symmetric key for Rackspace data bag decryption.
+default['barbican-monitoring']['credentials']['path'] = '/etc/chef/credentials_key'
