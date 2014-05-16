d2270f66c1b3da2631496c375ce88729336146a8
Igor Gueths <igor.gueths@rackspace.com>
2014-05-15 20:28:54 -0500

    Initial cleanup.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'barbican-monitoring'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures barbican-monitoring'
+maintainer       'Rackspace'
+maintainer_email 'igor.gueths@rackspace.com'
+license 'Apache 2.0'
+description 'Wraps the Rackspace Cloud Monitoring cookbook for use in the public cloud.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
