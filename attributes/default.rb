6b3949da2d0289537f4900e2f297c17a6130ca95
Igor Gueths <igor.gueths@rackspace.com>
2014-05-17 14:33:46 -0500

    More default attribute additions.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,13 @@
 default['barbican-monitoring']['install_agent'] = false
 default['barbican-monitoring']['notification_plan'] = ''
 default['barbican-monitoring']['node_type'] = ''
+# More default declarations for checks.
+default['barbican-monitoring']['check_entity_id'] = ""
+default['barbican-monitoring']['check_type'] = ""
+default['barbican-monitoring']['check_label'] = ""
+default['barbican-monitoring']['check_details'] = ""
+# Common alarm attributes.
+default['barbican-monitoring']['alarm_label'] = ""
+default['barbican-monitoring']['alarm_criteria'] = ""
+default['barbican-monitoring']['alarm_entity_id'] = ""
+default['barbican-monitoring']['alarm_notification_plan_id'] = ""
