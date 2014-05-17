eb2c705a692a1e3624ee33b164a2f2d335da4f94
Igor Gueths <igor.gueths@rackspace.com>
2014-05-17 14:18:34 -0500

    Remove symmetric key comment.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,4 @@
 # Default attributes for this wrapper.
 default['barbican-monitoring']['install_agent'] = false
 default['barbican-monitoring']['notification_plan'] = ''
-# Default path to our symmetric key for Rackspace data bag decryption.
 default['barbican-monitoring']['node_type'] = ''
