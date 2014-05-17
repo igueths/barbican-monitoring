bede71ef74a8be911778c4124f85b13b7f58422c
Igor Gueths <igor.gueths@rackspace.com>
2014-05-17 14:18:06 -0500

    Remove credentials attributes since we no longer need them.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,4 @@
 default['barbican-monitoring']['install_agent'] = false
 default['barbican-monitoring']['notification_plan'] = ''
 # Default path to our symmetric key for Rackspace data bag decryption.
-default['barbican-monitoring']['credentials_path'] = '/etc/chef/keys'
-default['barbican-monitoring']['credentials_filename'] = 'data_bag_key'
 default['barbican-monitoring']['node_type'] = ''
